5ebf9df96dea33182fd216e3bfe855a0c19f26b8
Chris Parkinson <unochild@gmail.com>
2016-09-15 09:02:10 +0100

    tidying up and adding notes to readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # sky-product-selector
+
+## Tools / Technologies used
+
+- ASP.NET 4.5.1 / MVC 5
+- NUnit
+- Moq
+- Fluent Assertions
+- Unity
+- Bootstrap
+- jQuery
+
+## Building / Running the solution
+
+- The solution requires VS 2013. Hitting F5 should restore missing nuget packages, build and run the solution.
+- Unit tests were written using NUnit and can either be run using the ReSharper test runner or a standalone NUnit test runner (pointing at the compiled assemblies).
+
+## Potential Improvements / Considerations (with more time)
+
+- Write web tests to test the controllers / actions (mocking the service calls)
+- UI tests - using selenium and coded UI tests using teststack seleno
+- Implement proper DAL using an ORM (possible EF) rather than in memory repository
+- Users selections currently not persisted (refreshing page clears them)
--- Solution/Sky.ProductSelector.Web/Scripts/app/product-selector.js
+++ Solution/Sky.ProductSelector.Web/Scripts/app/product-selector.js
@@ -19,9 +19,13 @@
             $j(document).on('change', self.settings.queryPaths.product, function() {
                 var selectedProducts = $j(self.settings.queryPaths.product + ':checked');
                 $j(self.settings.queryPaths.basketItems).empty();
-                $j.each(selectedProducts, function (i, item) {
-                    $j(self.settings.queryPaths.basketItems).append('<li>' + $j(item).val() + '</li>');
-                });
+                if (selectedProducts.length === 0) {
+                    $j(self.settings.queryPaths.basketItems).html('<li>Your basket is empty</li>');
+                } else {
+                    $j.each(selectedProducts, function (i, item) {
+                        $j(self.settings.queryPaths.basketItems).append('<li>' + $j(item).val() + '</li>');
+                    });
+                }
             });
         }
 
